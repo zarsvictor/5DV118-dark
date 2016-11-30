4f66575d96fd3a53a1da267d39e11ea2378cf91e
Zaars <dadisboy13@gmail.com>
2016-11-30 23:39:17 +0100

    Working on Memory.java

--- DARK_Assignment_2/src/com/company/Memory.java
+++ DARK_Assignment_2/src/com/company/Memory.java
@@ -5,7 +5,23 @@ package com.company;
  * A simulated memory class. Handles the memory for our simulator.
  */
 public class Memory {
-    public Memory(){
 
+    protected long[] memory;
+    protected long size;
+    protected long start;
+    protected long currentLocation;
+
+    /**
+     * Size in bytes (divisible by 4 so word aligned)
+     */
+    public Memory(int size, int start){
+        memory = new long[(size/4)];
+        this.size = size/4;
+        this.start = start;
+        this.currentLocation = start;
+    }
+
+    public long get(int loc) {
+        return memory[loc];
     }
 }
---
+++ DARK_Assignment_2/src/com/company/MipsSimulator.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Victor on 2016-11-29.
+ * The actual simulator.
+ */
+public class MipsSimulator {
+
+    public MipsSimulator() {
+
+    }
+
+    public void run(String path) {
+
+    }
+
+    public void step() {
+
+    }
+}
